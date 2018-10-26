6154c45e89cec16b4458b1288479c79eff5f74b8
Roma Lahodniuk <romalagod@gmail.com>
2018-10-26 14:03:26 +0300

    logger

--- FluxdayIO_ATQC/FluxdayIO_ATQC/TaskUI.cs
+++ FluxdayIO_ATQC/FluxdayIO_ATQC/TaskUI.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -32,18 +33,26 @@ namespace FluxdayIO_ATQC
         [Test]
         public void FirstTest1()
         {
+            // NUnit.Framework.Internal.Logger logger = new NUnit.Framework.Internal.Logger("first",InternalTraceLevel.Info,System.IO.TextWriter.Null);
+            // logger.Info("test log");
+            Console.WriteLine("Start test");
             driver.FindElement(By.Id("user_email")).Click();
             driver.FindElement(By.Id("user_email")).Clear();
             driver.FindElement(By.Id("user_email")).SendKeys("lead@fluxday.io");
+            Console.WriteLine("Field user name fill [ok]");
 
             driver.FindElement(By.Id("user_password")).Click();
             driver.FindElement(By.Id("user_password")).Clear();
             driver.FindElement(By.Id("user_password")).SendKeys("password");
+            Console.WriteLine("Field user password fill [ok]");
+
 
             driver.FindElement(By.CssSelector("#new_user > div.set > div.field-login > button")).Click();
 
             Assert.AreEqual("+Task",
-                driver.FindElement(By.CssSelector("body > div.fixed > nav > section > ul.right > li > a")).Text,"[OK]");            
+                driver.FindElement(By.CssSelector("body > div.fixed > nav > section > ul.right > li > a")).Text,"[OK]");
+            Console.WriteLine("Assert [ok]");
+
         }
     }
 }
